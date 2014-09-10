c4c8dbe280f6f717861a3348a8259f412c91ec46
Ankine <Ankine.mail.ru@mail.ru>
2014-09-11 00:32:52 +0300

    First commit

---
+++ Deak.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardsLib
+{
+    public delegate void LastCardDrawnHandler(Deak currentDeck);
+    public class Deak : ICloneable
+    {
+        public event LastCardDrawnHandler LastCardDrawn;
+        private Cards cards = new Cards();
+        private Random rnd1 = new Random();
+        //Додатковий конструктор для клонування колоди
+        private Deak(Cards newCards)
+        {
+            cards = newCards;
+        }
+        //Глибоке еопіювання
+        public object Clone()
+        {
+            Deak newDeak = new Deak(cards.Clone() as Cards);
+            return newDeak;
+        }
+        //Конструктор з встановленням туза старшим
+        public Deak(bool isAceHigh)
+            : this()
+        {
+            Card.isAceHigh = isAceHigh;
+        }
+        //Конструктор з встановленням козирю
+        public Deak(bool useTrumps, Suit trump)
+            : this()
+        {
+            Card.useTrumps = useTrumps;
+            Card.trump = trump;
+        }
+        public Deak(bool useTrumps, int trump)
+            : this()
+        {
+            Card.useTrumps = useTrumps;
+            Card.trump = (Suit)trump;
+        }
+        //Конструктор з старшим тузом і козирьом
+        public Deak(bool isAceHigh, bool useTrumps, Suit trump)
+            : this()
+        {
+            Card.isAceHigh = isAceHigh;
+            Card.useTrumps = useTrumps;
+            Card.trump = trump;
+        }
+        //Стандартний конструктор
+        public Deak()
+        {
+            for (int suitval = 0; suitval < 4; suitval++)
+            {
+                for (int rankval = 1; rankval < 14; rankval++)
+                {
+                    cards.Add(new Card((Suit)suitval, (Rank)rankval));
+                }
+            }
+        }
+        public Card GetCard(int cardNum)
+        {
+            if (cardNum >= 0 && cardNum <= cards.Count)
+            {
+                LastCardDrawn(this);
+                Card buffercard = cards[cardNum];
+                // cards.Remove(buffercard);
+                return buffercard;
+
+            }
+            else
+            {
+                throw new CardOutOfRangeException(cards.Clone() as Cards);
+            }
+        }
+        public Card GetRandomCard()
+        {
+            int randomval = rnd1.Next(cards.Count);
+            Card buferCard = new Card(cards[randomval].Suitcard, cards[randomval].Rankcard);
+            cards.Remove(cards[randomval]);
+            return buferCard;
+        }
+        public void Shuffle()
+        {
+            Cards cardsbufer = new Cards();
+            for (int i = 0; i < 51; i++)
+            {
+                cardsbufer.Add(this.GetRandomCard());
+            }
+            cards = cardsbufer;
+        }
+        public override string ToString()
+        {
+            string deakstr = "";
+            for (int i = 0; i < cards.Count; i++)
+            {
+                deakstr = string.Format("{0}\n{1}", deakstr, cards[i]);
+            }
+            return deakstr;
+        }
+    }
+
+    public class CardOutOfRangeException : Exception
+    {
+        private Cards deckContents;
+        public Cards DeckContents
+        {
+            get
+            {
+                return deckContents;
+            }
+        }
+        public CardOutOfRangeException(Cards sourceDeckContents)
+            : base("There are only 52 cards in deck.")
+        {
+            deckContents = sourceDeckContents;
+        }
+
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsLib
+{
+    public class Game
+    {
+        private int currentCard;
+        private Deak playDeak;
+        private Player[] players;
+        private Cards discardedCards;
+        public Game()
+        {
+            currentCard = 0;
+            playDeak = new Deak(true);
+            playDeak.LastCardDrawn += new LastCardDrawnHandler(Reshuffle);
+            playDeak.Shuffle();
+            discardedCards = new Cards();
+        }
+        private void Reshuffle(Deak currentDeak)
+        {
+            Console.WriteLine("Disarded cards reshuffle into deak");
+            currentDeak.Shuffle();
+            discardedCards.Clear();
+            currentCard = 0;
+        }
+        public void SetPlayers(Player[] newPlayers)
+        {
+            if (newPlayers.Length > 7)
+                throw new ArgumentException("A maximum of 7 players may play this game");
+
+            if (newPlayers.Length < 2)
+                throw new ArgumentException("A minimum of 2 players may play this game");
+            players = newPlayers;
+        }
+        private void DealHands()
+        {
+            for (int p = 0; p < players.Length; p++)
+            {
+                for (int c = 0; c < 7; c++)
+                {
+                    players[p].PlayHand.Add(playDeak.GetCard(currentCard++));
+                }
+            }
+        }
+        public int PlayGame()
+        {
+            //Проводити гру якшо існують гравці
+            if (players == null)
+                return -1;
+            //Перша роздача карт на руки
+            DealHands();
+            //Ініціалізація змінних
+            bool GameWon = false;
+            int currentPlayer;
+            Card playCard = playDeak.GetCard(currentCard++);
+            discardedCards.Add(playCard);
+            //головний цико гри
+            do
+            {
+                //прохід по всім гравцям
+                for (currentPlayer = 0; currentPlayer < players.Length; currentPlayer++)
+                {
+                    //виведення гравця і його карт
+                    Console.WriteLine("{0}'s turn.", players[currentPlayer].Name);
+                    Console.WriteLine("Current hand:");
+                    foreach (Card card in players[currentPlayer].PlayHand)
+                    {
+                        Console.WriteLine(card);
+                    }
+                    Console.WriteLine("Card in play: {0}", playCard);
+                    //Запрошення гравцю взяти карту
+                    bool inputOK = false;
+                    do
+                    {
+                        Console.WriteLine("Press T to take card in play or D to draw");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "t")
+                        {
+                            //Додавання карти на руки гравцю з столу
+                            Console.WriteLine("Drawn: {0}", playCard);
+                            if (discardedCards.Contains(playCard))
+                            {
+                                discardedCards.Remove(playCard);
+                            }
+                            players[currentPlayer].PlayHand.Add(playCard);
+                            inputOK = true;
+                        }
+                        if (input.ToLower() == "d")
+                        {
+                            Card newCard;
+                            bool cardIsAvailable;
+                            do
+                            {
+                                newCard = playDeak.GetCard(currentCard++);
+                                cardIsAvailable = !discardedCards.Contains(newCard);
+                                if (cardIsAvailable)
+                                {
+                                    foreach (Player testPlayer in players)
+                                    {
+                                        if (testPlayer.PlayHand.Contains(newCard))
+                                        {
+                                            cardIsAvailable = false;
+                                            break;
+                                        }
+                                    }
+                                }
+                            } while (!cardIsAvailable);
+
+                            //Додавання нової карти на руки гравцю
+                            Console.WriteLine("Drawn: {0}", newCard);
+                            players[currentPlayer].PlayHand.Add(newCard);
+                            inputOK = true;
+
+                        }
+
+                    } while (inputOK == false);
+                    //Відображення нової розкоадки кард з нумерацією
+                    Console.WriteLine("New hand:");
+                    for (int i = 0; i < players[currentPlayer].PlayHand.Count; i++)
+                    {
+                        Console.WriteLine("{0}: {1}", i + 1, players[currentPlayer].PlayHand[i]);
+                    }
+                    //Запрошення гравця відкинути якусь карту
+                    inputOK = false;
+                    int choice = -1;
+                    do
+                    {
+                        Console.WriteLine("Choose card to discard:");
+                        string input = Console.ReadLine();
+                        try
+                        {
+                            //Спроба перетворення
+                            choice = Convert.ToInt32(input);
+                            if ((choice > 0) && (choice <= 8))
+                                inputOK = true;
+                        }
+                        catch
+                        {
+                            //Ігнорувати
+                        }
+                    } while (inputOK == false);
+
+                    //Перенесення ссилки на видалену карту в PlayCard (викладування карти на стіл)
+                    //забрати карту в гравця і помістити в відкинуті карти
+
+                    playCard = players[currentPlayer].PlayHand[choice - 1];
+                    players[currentPlayer].PlayHand.RemoveAt(choice - 1);
+                    discardedCards.Add(playCard);
+                    Console.WriteLine("Discarding: {0}", playCard);
+                    Console.WriteLine();
+
+                    //Перевірка перемоги поточного гравця
+
+                    GameWon = players[currentPlayer].HasWon();
+                    if (GameWon == true) break;
+                }
+
+            } while (GameWon == false);
+            return currentPlayer;
+        }
+
+    }
+}
